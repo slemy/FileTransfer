0a90b63335e6d99da69953fc94ac84501b9b9751
slemy <slemy346@gmail.com>
2014-07-28 17:57:14 +0300

    Date: 28/07/2014 Author: Vlad Simion Description: 	-added layout for card fragment 	-added new util classes and methods

--- gen/com/vlad/simion/filetransfer/R.java
+++ gen/com/vlad/simion/filetransfer/R.java
@@ -10,6 +10,9 @@ package com.vlad.simion.filetransfer;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int DarkGray=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,30 +21,69 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int card_details_top_width=0x7f050007;
+        public static final int card_edit_height=0x7f050004;
+        public static final int card_edit_width=0x7f050005;
+        /**  Main Activity User Card Dimens 
+         */
+        public static final int card_image_height=0x7f050002;
+        public static final int card_image_width=0x7f050003;
+        public static final int card_top_layout_height=0x7f050006;
+        public static final int padding_10dp=0x7f050012;
+        public static final int padding_15dp=0x7f050013;
+        public static final int padding_1dp=0x7f05000d;
+        public static final int padding_20dp=0x7f050014;
+        public static final int padding_2dp=0x7f05000e;
+        public static final int padding_3dp=0x7f05000f;
+        public static final int padding_4dp=0x7f050010;
+        public static final int padding_5dp=0x7f050011;
+        public static final int padding_m1dp=0x7f050008;
+        public static final int padding_m2dp=0x7f050009;
+        public static final int padding_m3dp=0x7f05000a;
+        public static final int padding_m4dp=0x7f05000b;
+        public static final int padding_m5dp=0x7f05000c;
+        public static final int text_size_10sp=0x7f050016;
+        public static final int text_size_12sp=0x7f050017;
+        public static final int text_size_15sp=0x7f050018;
+        public static final int text_size_20sp=0x7f050019;
+        public static final int text_size_8sp=0x7f050015;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int card_item_divider=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int icod_card_edit=0x7f020002;
+        public static final int icon_user_no_profile_picture=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int fragment_default_title=0x7f080001;
-        public static final int main_activity_viewpager=0x7f080000;
+        public static final int action_settings=0x7f090008;
+        public static final int fragment_default_title=0x7f090007;
+        public static final int header_container=0x7f090006;
+        public static final int imv_card_edit=0x7f090002;
+        public static final int imv_card_profile_image=0x7f090001;
+        public static final int main_activity_viewpager=0x7f090000;
+        public static final int tv_card_profile_first_name=0x7f090003;
+        public static final int tv_card_profile_last_name=0x7f090004;
+        public static final int tv_card_profile_workplace=0x7f090005;
     }
     public static final class layout {
         public static final int activity_rftmain=0x7f030000;
-        public static final int fragment_default=0x7f030001;
+        public static final int card_layout=0x7f030001;
+        public static final int fragment_default=0x7f030002;
     }
     public static final class menu {
-        public static final int rftmain=0x7f070000;
+        public static final int rftmain=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050004;
-        public static final int app_name=0x7f050000;
-        public static final int title_section1=0x7f050001;
-        public static final int title_section2=0x7f050002;
-        public static final int title_section3=0x7f050003;
+        public static final int action_settings=0x7f060004;
+        public static final int app_name=0x7f060000;
+        public static final int card_firstname=0x7f060005;
+        public static final int card_lastname=0x7f060006;
+        public static final int card_workplace=0x7f060007;
+        public static final int title_section1=0x7f060001;
+        public static final int title_section2=0x7f060002;
+        public static final int title_section3=0x7f060003;
     }
     public static final class style {
         /** 
@@ -65,10 +107,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int style_card_profile_top_details=0x7f070002;
     }
 }
