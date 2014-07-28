60c00f53bde6df7aad8e8a807cd2219e57869e14
slemy <slemy346@gmail.com>
2014-07-28 11:35:57 +0300

    First app commit

---
+++ src/com/vlad/simion/filetransfer/ui/activities/RFTMainActivity.java
@@ -0,0 +1,18 @@
+package com.vlad.simion.filetransfer.ui.activities;
+
+import android.os.Bundle;
+import com.vlad.simion.filetransfer.R;
+import com.vlad.simion.filetransfer.ui.activities.base.RFTBaseActivity;
+
+public class RFTMainActivity extends RFTBaseActivity {
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_rftmain);
+	}
+
+
+
+}
